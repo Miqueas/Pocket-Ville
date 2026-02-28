07a4c5cc40eddce108a48eced1029ea7deee9ebc
Josué Martínez <JosueTech@proton.me>
2026-02-28 11:55:10 -0500

    Ft: Updated `main.dart` (see below):
    - Removed `MyApp` class
    - Created `PocketVilleApp` class
    - Set the default theme
    - Set the localizations and supported locales
    - Disabled the debug mode banner
    - Temporarily added the `OnboardingScreen`

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocket_ville/core/l10n/app_localizations.dart';
+import 'package:pocket_ville/features/onboarding/presentation/screens/onboarding_screen.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() => runApp(const ProviderScope(
+  child: PocketVilleApp(
+    home: OnboardingScreen(),
+  ),
+));
 
-final class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-final class MyHomePage extends StatefulWidget {
-  const MyHomePage({
+final class PocketVilleApp extends StatelessWidget {
+  const PocketVilleApp({
     super.key,
-    required this.title,
+    this.home,
   });
 
-  final String title;
+  final Widget? home;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-final class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() => setState(() => _counter++);
-
-  @override
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: .center,
-        children: [
-          const Text('You have pushed the button this many times:'),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-        ],
+  ThemeData _buildTheme(Color seedColor, Brightness brightness) {
+    final base = ThemeData.from(
+      useMaterial3: true,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: seedColor,
+        brightness: brightness,
+        dynamicSchemeVariant: .fidelity,
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: _incrementCounter,
-      tooltip: 'Increment',
-      child: const Icon(Icons.add),
-    ),
+    );
+
+    return base.copyWith(
+      textTheme: base.textTheme.apply(fontFamily: 'Poppins'),
+      brightness: brightness,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    home: home,
+    title: 'Pocket Ville',
+    theme: _buildTheme(Colors.blue, .light),
+    darkTheme: _buildTheme(Colors.blue, .dark),
+    themeMode: .system,
+    supportedLocales: AppLocalizations.supportedLocales,
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    debugShowCheckedModeBanner: false,
   );
 }
