1d547d5d11ab45cc519d0fb6ba56e83b9be92631
Josué Martínez <JosueTech@proton.me>
2026-02-28 11:46:11 -0500

    Ft: Added initial localizations

---
+++ lib/core/l10n/app_localizations_en.dart
@@ -0,0 +1,57 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get onboardingTitle01 => 'All Pokémon in one place';
+
+  @override
+  String get onboardingSubtitle01 =>
+      'Access a wide list of Pokémon from all generations created by Nintendo';
+
+  @override
+  String get onboardingTitle02 => 'Keep your Pokédex updated';
+
+  @override
+  String get onboardingSubtitle02 =>
+      'Register and save your profile, favorite Pokémon, settings, and much more in the app.';
+
+  @override
+  String get onboardingContinue => 'Continue';
+
+  @override
+  String get onboardingGetStarted => 'Get Started';
+
+  @override
+  String get pokedex => 'Pokédex';
+
+  @override
+  String get regions => 'Regions';
+
+  @override
+  String get favorites => 'Favorites';
+
+  @override
+  String get profile => 'Profile';
+
+  @override
+  String get somethingWentWrong => 'Something went wrong...';
+
+  @override
+  String get networkErrorMessage =>
+      'We were unable to load the information at this time. Please check your connection or try again later.';
+
+  @override
+  String get noFavoritesTitle =>
+      'You haven\'t marked any Pokémon as favorites.';
+
+  @override
+  String get noFavoritesSubtitle =>
+      'Tap on the heart icon of your favorite Pokémon and they will appear here.';
+}
---
+++ lib/core/l10n/app_localizations_es.dart
@@ -0,0 +1,56 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get onboardingTitle01 => 'Todos los Pokémon en un solo lugar';
+
+  @override
+  String get onboardingSubtitle01 =>
+      'Accede a una amplia lista de Pokémon de todas las generaciones creadas por Nintendo';
+
+  @override
+  String get onboardingTitle02 => 'Mantén tu Pokédex actualizada';
+
+  @override
+  String get onboardingSubtitle02 =>
+      'Regístrate y guarda tu perfil, Pokémon favoritos, configuraciones y mucho más en la aplicación';
+
+  @override
+  String get onboardingContinue => 'Continue';
+
+  @override
+  String get onboardingGetStarted => 'Get Started';
+
+  @override
+  String get pokedex => 'Pokédex';
+
+  @override
+  String get regions => 'Regiones';
+
+  @override
+  String get favorites => 'Favoritos';
+
+  @override
+  String get profile => 'Perfil';
+
+  @override
+  String get somethingWentWrong => 'Algo salió mal...';
+
+  @override
+  String get networkErrorMessage =>
+      'No pudimos cargar la información en este momento. Verifica tu conexión o intenta nuevamente más tarde.';
+
+  @override
+  String get noFavoritesTitle => 'No has marcado ningún Pokémon como favorito.';
+
+  @override
+  String get noFavoritesSubtitle =>
+      'Haz clic en el ícono de corazón de tus Pokémon favoritos y aparecerán aquí.';
+}
