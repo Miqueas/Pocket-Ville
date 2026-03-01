6af6fdfda4c67fc79c9141914a9d60996628e4b2
Josué Martínez <JosueTech@proton.me>
2026-03-01 09:06:57 -0500

    Ft: Now using `BaseScreen` on `OnboardingScreen`

--- lib/features/onboarding/presentation/screens/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_ville/core/presentation/screens/base_screen.dart';
 import 'package:pocket_ville/core/presentation/widgets/primary_button.dart';
 import 'package:pocket_ville/features/onboarding/presentation/widgets/onboarding_animated_button_text.dart';
 import 'package:pocket_ville/features/onboarding/presentation/widgets/onboarding_page_01.dart';
@@ -22,33 +23,36 @@ final class _OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.white,
-    body: SafeArea(
-      child: Padding(
-        padding: const .all(16),
-        child: Column(
-          spacing: 16,
-          children: [
-            Expanded(child: PageView(
-              controller: _controller,
-              children: const [
-                OnboardingPage01(),
-                OnboardingPage02(),
-              ],
-            ),),
-            OnboardingPageIndicators(controller: _controller,),
-            PrimaryButton(
-              width: .infinity,
-              onPressed: () => _controller.nextPage(
-                curve: Curves.easeInOut,
-                duration: const Duration(milliseconds: 150),
-              ),
-              child: OnboardingAnimatedButtonText(controller: _controller,),
-            ),
-          ],
-        ),
+  Widget build(BuildContext context) => BaseScreen(child: Column(
+    spacing: 16,
+    children: [
+      Expanded(child: PageView(
+        controller: _controller,
+        children: const [
+          OnboardingPage01(),
+          OnboardingPage02(),
+        ],
+      ),),
+      OnboardingPageIndicators(controller: _controller,),
+      PrimaryButton(
+        width: .infinity,
+        onPressed: () {
+          if (!_controller.hasClients) return;
+          if (_controller.page == null) return;
+
+          if (_controller.page! < .5) {
+            _controller.nextPage(
+              curve: Curves.easeInOut,
+              duration: const Duration(milliseconds: 150),
+            );
+          }
+
+          if (_controller.page! > .5) {
+            // TODO: Navigate to the main screen
+          }
+        },
+        child: OnboardingAnimatedButtonText(controller: _controller,),
       ),
-    ),
-  );
+    ],
+  ),);
 }
