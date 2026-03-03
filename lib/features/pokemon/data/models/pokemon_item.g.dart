02ca1ac55ddeee9a56619f932a87d3c906ca173a
Josué Martínez <JosueTech@proton.me>
2026-03-03 18:36:18 -0500

    Ft: Created `PokemonItem`

---
+++ lib/features/pokemon/data/models/pokemon_item.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pokemon_item.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_PokemonItem _$PokemonItemFromJson(Map<String, dynamic> json) =>
+    _PokemonItem(name: json['name'] as String, url: json['url'] as String);
+
+Map<String, dynamic> _$PokemonItemToJson(_PokemonItem instance) =>
+    <String, dynamic>{'name': instance.name, 'url': instance.url};
