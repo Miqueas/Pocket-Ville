f520218234017c9e28933086d8af45ff4516a464
Josué Martínez <JosueTech@proton.me>
2026-03-04 23:40:05 -0500

    Ft: Created `HomeScreen`

---
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pocket_ville/core/extensions/gap.dart';
+import 'package:pocket_ville/core/presentation/widgets/loading.dart';
+import 'package:pocket_ville/features/pokemon/presentation/providers/pokemon_list_provider.dart';
+import 'package:pocket_ville/features/pokemon/presentation/widgets/pokemon_card.dart';
+import 'package:pocket_ville/features/home/presentation/widgets/home_search_field.dart';
+
+final class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonList = ref.watch(pokemonListProvider);
+
+    return Column(
+      spacing: 16,
+      children: [
+        const HomeSearchField(),
+        Expanded(child: switch (pokemonList) {
+          AsyncValue(:final error?) => Center(child: Text('Error: $error')),
+          AsyncValue(:final value?) => ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(
+              overscroll: false,
+            ),
+            child: NotificationListener<ScrollNotification>(
+              onNotification: (notification) {
+                final position = notification.metrics.pixels;
+                final maxScroll = notification.metrics.maxScrollExtent;
+
+                if (position >= (maxScroll * .9)) {
+                  ref.read(pokemonListProvider.notifier).loadMore();
+                }
+
+                return false;
+              },
+              child: ListView.separated(
+                itemCount: value.length + (pokemonList.isLoading ? 1 : 0),
+                cacheExtent: 100,
+                itemBuilder: (_, index) {
+                  if (index >= value.length) {
+                    return const Center(child: Loading(),);
+                  }
+
+                  return PokemonCard(pokemon: value[index],);
+                },
+                separatorBuilder: (_, _) => 12.h,
+              ),
+            ),
+          ),
+          _ => const Center(child: Loading(),),
+        },),
+      ],
+    );
+  }
+}
