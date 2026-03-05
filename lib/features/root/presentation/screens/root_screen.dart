b9ab0bac7c407900cb895785316859b0d1af5696
Josué Martínez <JosueTech@proton.me>
2026-03-04 23:40:48 -0500

    Ft: Created `RootScreen`

---
+++ lib/features/root/presentation/screens/root_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:pocket_ville/core/presentation/screens/base_scaffold.dart';
+import 'package:pocket_ville/core/presentation/widgets/navigation_tab_bar.dart';
+import 'package:pocket_ville/features/home/presentation/screens/home_screen.dart';
+import 'package:pocket_ville/features/regions/presentation/screens/regions_screen.dart';
+
+final class RootScreen extends StatefulWidget {
+  const RootScreen({super.key});
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+final class _RootScreenState extends State<RootScreen> {
+  var _pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) => BaseScaffold(
+    body: IndexedStack(
+      index: _pageIndex,
+      children: const [
+        HomeScreen(),
+        RegionsScreen(),
+        Placeholder(),
+        Placeholder(),
+      ],
+    ),
+    bottomNavigationBar: NavigationTabBar(
+      activeBranch: .values[_pageIndex],
+      onBranchSelected: (branch) => setState(() => _pageIndex = branch.index),
+    ),
+  );
+}
