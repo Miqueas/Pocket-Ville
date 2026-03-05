4fe3ea2b6d0e70e5f9ab5d9cf82f25de5410a5da
Josué Martínez <JosueTech@proton.me>
2026-03-04 23:02:51 -0500

    Ft: Changes to `NavigationTabBar` (see below)
    - Created `NavigationTabBarBranch` enum
    - Added `activeBranch` property
    - Added `onBranchSelected` property
    - Now using `AppLocalizations`
    - Properly implemented the logic for the `active` value for the items
    - Also properly using `onBranchSelected` on each item's `onTap` property
    - Added dark mode support

--- lib/core/presentation/widgets/navigation_tab_bar.dart
+++ lib/core/presentation/widgets/navigation_tab_bar.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:pocket_ville/core/l10n/app_localizations.dart';
 import 'package:pocket_ville/core/presentation/widgets/navigation_tab_bar_item.dart';
 
+enum NavigationTabBarBranch {
+  home,
+  globe,
+  favorites,
+  profile,
+}
+
 final class NavigationTabBar extends StatelessWidget {
-  const NavigationTabBar({ super.key, });
+  const NavigationTabBar({
+    super.key,
+    required this.activeBranch,
+    required this.onBranchSelected,
+  });
+
+  final NavigationTabBarBranch activeBranch;
+  final ValueChanged<NavigationTabBarBranch> onBranchSelected;
 
   @override
   Widget build(BuildContext context) {
+    final brightness = Theme.of(context).brightness;
     final viewInsets = MediaQuery.viewInsetsOf(context);
     final viewPadding = MediaQuery.paddingOf(context);
 
@@ -14,41 +30,53 @@ final class NavigationTabBar extends StatelessWidget {
         bottom: viewInsets.bottom + viewPadding.bottom + 16,
       ),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: switch (brightness) {
+          .dark => const Color(0xFF16161B),
+          .light => Colors.white,
+        },
         borderRadius: const .only(
           topLeft: .circular(16),
           topRight: .circular(16),
         ),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withValues(alpha: .2),
+            color: Colors.black.withValues(
+              alpha: switch (brightness) {
+                .dark => .8,
+                .light => .2,
+              },
+            ),
             offset: const Offset(0, -1),
             blurRadius: 4,
           ),
         ],
       ),
-      child: const Row(
+      child: Row(
         mainAxisAlignment: .spaceAround,
         children: [
           NavigationTabBarItem(
             icon: 'house',
-            label: 'Pokedex',
-            active: true,
+            label: AppLocalizations.of(context)!.pokedex,
+            active: activeBranch == .home,
+            onTap: () => onBranchSelected(.home),
           ),
           NavigationTabBarItem(
             icon: 'globe',
-            label: 'Regions',
-            active: false,
+            label: AppLocalizations.of(context)!.regions,
+            active: activeBranch == .globe,
+            onTap: () => onBranchSelected(.globe),
           ),
           NavigationTabBarItem(
             icon: 'heart',
-            label: 'Favorites',
-            active: false,
+            label: AppLocalizations.of(context)!.favorites,
+            active: activeBranch == .favorites,
+            onTap: () => onBranchSelected(.favorites),
           ),
           NavigationTabBarItem(
             icon: 'user',
-            label: 'Profile',
-            active: false,
+            label: AppLocalizations.of(context)!.profile,
+            active: activeBranch == .profile,
+            onTap: () => onBranchSelected(.profile),
           ),
         ],
       ),
