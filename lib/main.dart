60740fd0f8fc61ffd3ee5f32629825fee2bb3b27
Josué Martínez <JosueTech@proton.me>
2026-03-05 00:16:24 -0500

    Ft: Modified `main.dart` (see below)
    - Removed `SplashScreen` from `PocketVilleApp.home` property
    - Added `AnnotatedRegion` for system UI colors & theme integration with the app
    - Added some initial routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocket_ville/core/l10n/app_localizations.dart';
+import 'package:pocket_ville/features/home/presentation/screens/home_screen.dart';
+import 'package:pocket_ville/features/onboarding/presentation/screens/onboarding_screen.dart';
+import 'package:pocket_ville/features/root/presentation/screens/root_screen.dart';
 import 'package:pocket_ville/features/splash/presentation/screens/splash_screen.dart';
 
-void main() => runApp(const ProviderScope(
-  child: PocketVilleApp(
-    home: SplashScreen(),
-  ),
-));
+void main() => runApp(const ProviderScope(child: PocketVilleApp(),),);
 
 final class PocketVilleApp extends StatelessWidget {
   const PocketVilleApp({
@@ -34,14 +34,29 @@ final class PocketVilleApp extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    home: home,
-    title: 'Pocket Ville',
-    theme: _buildTheme(Colors.blue, .light),
-    darkTheme: _buildTheme(Colors.blue, .dark),
-    themeMode: .system,
-    supportedLocales: AppLocalizations.supportedLocales,
-    localizationsDelegates: AppLocalizations.localizationsDelegates,
-    debugShowCheckedModeBanner: false,
+  Widget build(BuildContext context) => AnnotatedRegion(
+    value: const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: .dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: .dark,
+    ),
+    child: MaterialApp(
+      home: home,
+      title: 'Pocket Ville',
+      theme: _buildTheme(Colors.blue, .light),
+      themeMode: .system,
+      darkTheme: _buildTheme(Colors.blue, .dark),
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/splash',
+      routes: {
+        '/splash': (_) => const SplashScreen(),
+        '/onboarding': (_) => const OnboardingScreen(),
+        '/': (_) => const RootScreen(),
+      },
+    ),
   );
 }
