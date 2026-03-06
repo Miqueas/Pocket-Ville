990a0d15fdb1e18d6052ebe05931f8b8067ae68a
Josué Martínez <JosueTech@proton.me>
2026-03-06 12:02:51 -0500

    Fix: Missing `GestureDetector` on `FavoriteButton`

--- lib/core/presentation/widgets/favorite_button.dart
+++ lib/core/presentation/widgets/favorite_button.dart
@@ -11,25 +11,29 @@ final class FavoriteButton extends StatelessWidget {
   final VoidCallback onPressed;
 
   @override
-  Widget build(BuildContext context) => Container(
-    padding: const .all(4),
-    decoration: BoxDecoration(
-      shape: .circle,
-      color: const HSLColor.fromAHSL(.6, 0, 0, 0.46).toColor(),
-      border: .all(
-        width: 2,
-        color: const HSLColor.fromAHSL(1, 0, 0, .98).toColor()
+  Widget build(BuildContext context) => GestureDetector(
+    behavior: .opaque,
+    onTap: onPressed,
+    child: Container(
+      padding: const .all(4),
+      decoration: BoxDecoration(
+        shape: .circle,
+        color: const HSLColor.fromAHSL(.6, 0, 0, 0.46).toColor(),
+        border: .all(
+          width: 2,
+          color: const HSLColor.fromAHSL(1, 0, 0, .98).toColor()
+        ),
       ),
-    ),
-    child: Icon(
-      switch (favorited) {
-        true => Icons.favorite,
-        false => Icons.favorite_outline,
-      },
-      color: switch (favorited) {
-        true => const HSLColor.fromAHSL(1, 1, .77, .55).toColor(),
-        false => Colors.white,
-      }
+      child: Icon(
+        switch (favorited) {
+          true => Icons.favorite,
+          false => Icons.favorite_outline,
+        },
+        color: switch (favorited) {
+          true => const HSLColor.fromAHSL(1, 1, .77, .55).toColor(),
+          false => Colors.white,
+        }
+      ),
     ),
   );
 }
