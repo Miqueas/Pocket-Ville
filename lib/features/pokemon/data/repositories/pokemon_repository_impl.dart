eb3e776bb77ce474b78d9ad95496e3b8b5efee99
Josué Martínez <JosueTech@proton.me>
2026-03-06 00:48:44 -0500

    Ft: Updated `PokemonRepositoryImpl` (see below)
    - Switched to GraphQL API
    - Properly implemented `getPokemons` from `PokemonRepository`

--- lib/features/pokemon/data/repositories/pokemon_repository_impl.dart
+++ lib/features/pokemon/data/repositories/pokemon_repository_impl.dart
@@ -1,35 +1,86 @@
 import 'package:dio/dio.dart';
-import 'package:pocket_ville/features/pokemon/data/models/pokemon_item.dart';
+import 'package:pocket_ville/features/pokemon/data/models/pokemon.dart';
 import 'package:pocket_ville/features/pokemon/domain/repositories/pokemon_repository.dart';
 
 final class PokemonRepositoryImpl implements PokemonRepository {
-  final Dio _dio;
-
   const PokemonRepositoryImpl(this._dio);
 
-  @override
-  Future<Map<String, dynamic>> getPokemonData<T>(T idOrName) async {
-    assert(
-      idOrName is int || idOrName is String,
-      '`idOrName` must be int or String'
-    );
+  final Dio _dio;
 
-    final response = await _dio.get('pokemon/$idOrName');
-    return response.data;
+  static const _gqlQuery = '''
+query ListPokemon(\$limit: Int, \$offset: Int) {
+  pokemons: pokemon(limit: \$limit, offset: \$offset, order_by: {id: asc}) {
+    id
+    name
+    height
+    weight
+    sprites: pokemonsprites {
+      front: sprites(path: "front_default")
+      showdown: sprites(path: "other.showdown.front_default")
+    }
+    types: pokemontypes(order_by: {id: asc}) {
+      type {
+        name
+        weaknesses: TypeefficaciesByTargetTypeId(where: {damage_factor: {_gt: 100}}) {
+          attacking_type: type {
+            name
+          }
+        }
+      }
+    }
+    abilities: pokemonabilities {
+      ability {
+        name
+        localizations: abilitynames(where: {language_id: {_in: [7, 9]}}) {
+          name
+          language {
+            name
+          }
+        }
+      }
+    }
+    species: pokemonspecy {
+      gender_rate
+      genera: pokemonspeciesnames(where: {language_id: {_in: [7, 9]}}) {
+        name
+        genus
+        language {
+          name
+        }
+      }
+      descriptions: pokemonspeciesflavortexts(
+        where: {language_id: {_in: [7, 9]}}
+        order_by: {version_id: desc}
+      ) {
+        version_id
+        flavor_text
+        language {
+          name
+        }
+      }
+    }
   }
+}
+''';
 
   @override
-  Future<List<PokemonItem>> getPokemonList({int? limit, int? offset}) async {
-    final Response response = await _dio.get(
-      'pokemon',
-      queryParameters: {
-        'limit': ?limit,
-        'offset': ?offset,
+  Future<List<Pokemon>> getPokemons({int? limit, int? offset}) async {
+    final response = await _dio.post(
+      '/v1beta2',
+      data: {
+        'query': _gqlQuery,
+        'variables': {
+          'limit': ?limit,
+          'offset': ?offset,
+        },
       },
     );
 
-    final List results = response.data['results'];
+    final pokemons = response.data['data']['pokemons']
+      .map((pokemon) => Pokemon.fromJson(pokemon))
+      .toList()
+      .cast<Pokemon>();
 
-    return results.map((e) => PokemonItem.fromJson(e)).toList();
+    return pokemons;
   }
 }
