e6e09446fecab586d9594b26f8c87975e16ad12a
Josué Martínez <JosueTech@proton.me>
2026-03-06 16:26:17 -0500

    Ft: Now using `FavoritesView` on `FavoritesScreen`

--- lib/features/favorites/presentation/screens/favorites_screen.dart
+++ lib/features/favorites/presentation/screens/favorites_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pocket_ville/core/extensions/gap.dart';
 import 'package:pocket_ville/core/l10n/app_localizations.dart';
+import 'package:pocket_ville/core/presentation/widgets/error_card.dart';
 import 'package:pocket_ville/core/presentation/widgets/loading.dart';
+import 'package:pocket_ville/core/presentation/widgets/retry_button.dart';
 import 'package:pocket_ville/features/favorites/presentation/providers/favorites_provider.dart';
-import 'package:pocket_ville/features/pokemon/presentation/providers/pokemon_by_id_provider.dart';
-import 'package:pocket_ville/features/pokemon/presentation/widgets/pokemon_card.dart';
+import 'package:pocket_ville/features/favorites/presentation/widgets/favorites_view.dart';
 
 final class FavoritesScreen extends ConsumerWidget {
   const FavoritesScreen({super.key,});
@@ -25,47 +25,14 @@ final class FavoritesScreen extends ConsumerWidget {
           ),
         ),
         Expanded(child: switch (favoriteIds) {
-          AsyncValue(:final error?) => Center(child: Text('Error: $error')),
-          AsyncValue(:final value?) => Builder(builder: (context) {
-            final favoritePokemon = ref.watch(pokemonByIdProvider(value));
-
-            return switch (favoritePokemon) {
-              AsyncValue(:final error?) => Center(child: Text('Error: $error')),
-              AsyncValue(:final value?) => NotificationListener<ScrollNotification>(
-                onNotification: (notification) {
-                  final position = notification.metrics.pixels;
-                  final maxScroll = notification.metrics.maxScrollExtent;
-              
-                  if (position >= (maxScroll * .9)) {
-                    // ref.read(pokemonByIdProvider(value).notifier).loadMore();
-                  }
-              
-                  return false;
-                },
-                child: ListView.separated(
-                  itemCount: value.length,
-                  cacheExtent: 100,
-                  itemBuilder: (_, index) => Dismissible(
-                    key: Key(value[index].id.toString()),
-                    direction: .endToStart,
-                    onDismissed: (direction) {
-                      final notifier = ref.read(favoritesProvider.notifier);
-                      notifier.remove(value[index].id);
-                    },
-                    background: Container(
-                      color: Colors.red,
-                      padding: const .only(right: 32),
-                      alignment: .centerRight,
-                      child: const Icon(Icons.delete, color: Colors.white,),
-                    ),
-                    child: PokemonCard(pokemon: value[index],),
-                  ),
-                  separatorBuilder: (_, _) => 12.h,
-                ),
-              ),
-              _ => const Center(child: Loading(),),
-            };
-          }),
+          AsyncValue(error: != null) => ErrorCard(
+            title: AppLocalizations.of(context)!.somethingWentWrong,
+            message: AppLocalizations.of(context)!.networkErrorMessage,
+            child: RetryButton(
+              onTap: () {},
+            ),
+          ),
+          AsyncValue(:final value?) => FavoritesView(favoriteIds: value),
           _ => const Center(child: Loading(),),
         }),
       ],
