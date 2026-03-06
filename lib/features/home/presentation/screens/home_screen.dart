30f1e1b1f77246b8f3664e625957ee5bc295cf70
Josué Martínez <JosueTech@proton.me>
2026-03-06 16:55:47 -0500

    Ft: Modified `HomeScreen` (see below)
    - Added `HomeFilterButton`
    - Now using `ErrorCard`
    - Some other minor changes

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocket_ville/core/extensions/gap.dart';
+import 'package:pocket_ville/core/l10n/app_localizations.dart';
+import 'package:pocket_ville/core/presentation/widgets/error_card.dart';
 import 'package:pocket_ville/core/presentation/widgets/loading.dart';
+import 'package:pocket_ville/core/presentation/widgets/retry_button.dart';
+import 'package:pocket_ville/features/home/presentation/widgets/home_filter_button.dart';
 import 'package:pocket_ville/features/pokemon/presentation/providers/pokemon_listing_provider.dart';
 import 'package:pocket_ville/features/pokemon/presentation/widgets/pokemon_card.dart';
 import 'package:pocket_ville/features/home/presentation/widgets/home_search_field.dart';
@@ -12,37 +16,51 @@ final class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final pokemonListing = ref.watch(pokemonListingProvider);
+    final isLoading = pokemonListing.isLoading;
 
     return Column(
       spacing: 16,
       children: [
-        const HomeSearchField(),
+        const Row(
+          spacing: 12,
+          children: [
+            Expanded(child: HomeSearchField()),
+            HomeFilterButton(),
+          ],
+        ),
         Expanded(child: switch (pokemonListing) {
-          AsyncValue(:final error?) => Center(child: Text('Error: $error')),
-          AsyncValue(:final value?) => NotificationListener<ScrollNotification>(
-            onNotification: (notification) {
-              final position = notification.metrics.pixels;
-              final maxScroll = notification.metrics.maxScrollExtent;
-          
-              if (position >= (maxScroll * .9)) {
-                ref.read(pokemonListingProvider.notifier).loadMore();
-              }
-          
-              return false;
-            },
-            child: ListView.separated(
-              itemCount: value.length + (pokemonListing.isLoading ? 1 : 0),
-              cacheExtent: 100,
-              itemBuilder: (_, index) {
-                if (index >= value.length) {
-                  return const Center(child: Loading(),);
-                }
-          
-                return PokemonCard(pokemon: value[index],);
-              },
-              separatorBuilder: (_, _) => 12.h,
+          AsyncValue(error: != null) => ErrorCard(
+            title: AppLocalizations.of(context)!.somethingWentWrong,
+            message: AppLocalizations.of(context)!.networkErrorMessage,
+            child: RetryButton(
+              onTap: () => ref.invalidate(pokemonListingProvider)
             ),
           ),
+          AsyncValue(:final value?) =>
+            NotificationListener<ScrollNotification>(
+              onNotification: (notification) {
+                final position = notification.metrics.pixels;
+                final maxScroll = notification.metrics.maxScrollExtent;
+            
+                if (position >= (maxScroll * .9)) {
+                  ref.read(pokemonListingProvider.notifier).loadMore();
+                }
+            
+                return false;
+              },
+              child: ListView.separated(
+                itemCount: value.length + (isLoading ? 1 : 0),
+                cacheExtent: 100,
+                itemBuilder: (_, index) {
+                  if (index >= value.length) {
+                    return const Center(child: Loading(),);
+                  }
+            
+                  return PokemonCard(pokemon: value[index]);
+                },
+                separatorBuilder: (_, _) => 12.h,
+              ),
+            ),
           _ => const Center(child: Loading(),),
         },),
       ],
