39314ac49217753a61d8d5bbbcee464a66e8bb8b
Josué Martínez <JosueTech@proton.me>
2026-03-06 17:41:18 -0500

    Ft: Minor changes to `PokemonRepository`

--- lib/features/pokemon/domain/repositories/pokemon_repository.dart
+++ lib/features/pokemon/domain/repositories/pokemon_repository.dart
@@ -1,6 +1,14 @@
 import 'package:pocket_ville/features/pokemon/data/models/pokemon.dart';
 
 abstract class PokemonRepository {
-  Future<List<Pokemon>> getPokemonByIds(Iterable<int> ids);
-  Future<List<Pokemon>> getPokemonListing({int? limit, int? offset});
+  Future<List<Pokemon>> getPokemonByIds({
+    required Iterable<int> ids,
+    int? limit,
+    int? offset,
+  });
+
+  Future<List<Pokemon>> getPokemonListing({
+    int? limit,
+    int? offset,
+  });
 }
