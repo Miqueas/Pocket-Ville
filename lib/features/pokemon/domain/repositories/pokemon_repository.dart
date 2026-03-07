7744a6678b1f1fe68333dc049f7ca5e9cfde1d6e
Josué Martínez <JosueTech@proton.me>
2026-03-06 19:01:20 -0500

    Ft: Added `searchPokemon` to `PokemonRepository`

--- lib/features/pokemon/domain/repositories/pokemon_repository.dart
+++ lib/features/pokemon/domain/repositories/pokemon_repository.dart
@@ -1,6 +1,12 @@
 import 'package:pocket_ville/features/pokemon/data/models/pokemon.dart';
 
 abstract class PokemonRepository {
+  Future<List<Pokemon>> searchPokemon({
+    required String query,
+    int? limit,
+    int? offset,
+  });
+
   Future<List<Pokemon>> getPokemonByIds({
     required Iterable<int> ids,
     int? limit,
