640ce71e588d9055138d858f44b1d13d3d4164f1
Josué Martínez <JosueTech@proton.me>
2026-03-08 18:36:49 -0500

    Ft: Created initial `PokemonRestRepository`

---
+++ lib/features/pokemon/data/repositories/pokemon_rest_repository.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:pocket_ville/features/pokemon/data/models/pokemon.dart';
+import 'package:pocket_ville/features/pokemon/domain/repositories/pokemon_repository.dart';
+
+final class PokemonRestRepository implements PokemonRepository {
+  final Dio _dio;
+
+  const PokemonRestRepository(this._dio);
+
+  @override
+  Future<List<Pokemon>> searchPokemon({
+    required String query,
+    int? limit,
+    int? offset,
+  }) => throw UnimplementedError('This repository is not implemented yet');
+
+  @override
+  Future<List<Pokemon>> getPokemonByIds({
+    required Iterable<int> ids,
+    int? limit,
+    int? offset,
+  }) => throw UnimplementedError('This repository is not implemented yet');
+
+  @override
+  Future<List<Pokemon>> getPokemonListing({
+    int? limit,
+    int? offset,
+  }) async {
+    final response = await _dio.get(
+      '/pokemon',
+      queryParameters: {
+        'limit': limit,
+        'offset': offset,
+      },
+    );
+
+    final pokemons = response.data['results']
+      .map((pokemon) => Pokemon.fromJson(pokemon))
+      .toList()
+      .cast<Pokemon>();
+
+    return pokemons;
+  }
+}
