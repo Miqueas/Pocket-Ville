31d4af5eec5133a8f98f793c9c630e8b9a552401
Josué Martínez <JosueTech@proton.me>
2026-03-09 02:30:14 -0500

    Ft: Modified `PokemonElementPill` (see below)
    - Renamed to `PokemonTypePill`
    - Modified `element` property:
    	- Renamed to `type`
    	- Switched type to `PokemonType`
    - Now using the localizations from `type`

--- lib/features/pokemon/presentation/widgets/pokemon_element_pill.dart
+++ lib/features/pokemon/presentation/widgets/pokemon_element_pill.dart
@@ -1,51 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:pocket_ville/core/enums/pokemon_element.dart';
 import 'package:pocket_ville/core/l10n/app_localizations.dart';
 import 'package:pocket_ville/core/presentation/widgets/dynamic_svg_asset.dart';
+import 'package:pocket_ville/features/pokemon/data/models/pokemon_type.dart';
 
-final class PokemonElementPill extends StatelessWidget {
-  const PokemonElementPill({
+final class PokemonTypePill extends StatelessWidget {
+  const PokemonTypePill({
     super.key,
-    required this.element,
+    required this.type,
   });
 
-  final PokemonElement element;
+  final PokemonType type;
 
   @override
-  Widget build(BuildContext context) => Container(
-    padding: const EdgeInsets.all(6).copyWith(right: 12,),
-    decoration: BoxDecoration(
-      color: element.color,
-      borderRadius: BorderRadius.circular(32),
-    ),
-    child: Row(
-      spacing: 6,
-      mainAxisSize: .min,
-      children: [
-        Container(
-          padding: const .all(4),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            shape: BoxShape.circle,
+  Widget build(BuildContext context) {
+    final localeName = AppLocalizations.of(context)!.localeName;
+
+    return Container(
+      padding: const EdgeInsets.all(6).copyWith(right: 12,),
+      decoration: BoxDecoration(
+        color: type.element.color,
+        borderRadius: BorderRadius.circular(32),
+      ),
+      child: Row(
+        spacing: 6,
+        mainAxisSize: .min,
+        children: [
+          Container(
+            padding: const .all(4),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+            child: DynamicSvgAsset(
+              asset: 'assets/icons/${type.element.name}.svg',
+              replacements: { RegExp(r'#FAFAFA'): type.element.color, },
+            ),
           ),
-          child: DynamicSvgAsset(
-            asset: 'assets/icons/${element.name}.svg',
-            replacements: { RegExp(r'#FAFAFA'): element.color, },
+          Text(
+            type.localizations.firstWhere((l) => l.language == localeName).name,
+            style: TextStyle(
+              // Ensures to use the right text color based on the background color
+              color: switch (type.element.color.computeLuminance() < .5) {
+                true => Colors.white,
+                false => const Color(0xFF121212),
+              },
+              fontSize: 12,
+              fontWeight: .w500,
+            ),
           ),
-        ),
-        Text(
-          AppLocalizations.of(context)!.pokemonType(element.name),
-          style: TextStyle(
-            // Ensures to use the right text color based on the background color
-            color: switch (element.color.computeLuminance() < .5) {
-              true => Colors.white,
-              false => const Color(0xFF121212),
-            },
-            fontSize: 12,
-            fontWeight: .w500,
-          ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
+  }
 }
